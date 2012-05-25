f9229d6d9e78c00deac0f0ca1fde467d7c3ec9fa
Arturo Puente <arturopuentevc@gmail.com>
2012-05-25 04:38:13 -0500

    Huh, por fin

--- spec/spec/support/capybara.rb
+++ spec/spec/support/capybara.rb
@@ -1,6 +1,5 @@
 require 'capybara/poltergeist'
 
-# Capybara.javascript_driver = :poltergeist
 Capybara.configure do |config|
   config.javascript_driver = :poltergeist
   config.current_driver = :poltergeist
